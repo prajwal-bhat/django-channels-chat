a12f047ccea5acbad64c384abe6fefa6c43b750c
Prajwal <prajwalbhatmr@gmail.com>
2021-07-27 19:24:15 +0530

    Settings import issue fix

--- justchat/asgi.py
+++ justchat/asgi.py
@@ -8,6 +8,8 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
 import os
+import django
+django.setup()
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
