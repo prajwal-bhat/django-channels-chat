e08fea5320f50c702eee20b0819d61c1e8a61c67
Prajwal <prajwalbhatmr@gmail.com>
2021-07-30 21:22:22 +0530

    Fixing URL routing issue

--- chat/urls.py
+++ chat/urls.py
@@ -7,6 +7,7 @@ app_name = 'chat'
 urlpatterns = [
     path('', views.index, name='index'),
     path('accounts/login/', views.redirect_to_admin_page, name="home"),
+    path('chat/', views.redirect_to_admin_page, name="home"),
     path('chat/<str:room_name>/', views.room, name='room'),
     path('api/chats/<str:room_name>/<int:pk>/', views.get_chat_messages_paginated, name='chats'),
 ]
